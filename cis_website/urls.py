bec651ee764987f70544a544ec86ff7720256b07
BhargavaramMuppuru <56501891+BhargavaramMuppuru@users.noreply.github.com>
2021-09-08 10:20:59 +0530

    initialcommit

---
+++ cis_website/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('events', views.events, name='events'),
+    path('blog', views.blog, name='blog'),
+    path('board', views.board, name='board'),
+    path('boards', views.blog, name='boards'),
+    path('contact_us', views.contact, name='contact'),
+    path('upload_blog', views.upload_blog, name='upload_blog'),
+    path('robots.txt', views.robots),
+    path('sitemap.xml', views.sitemap),
+]
